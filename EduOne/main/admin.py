230e4bfc584d2c6fdf0904312c9ecc95a6c01765
ibenrawkss <benben-x3@hotmail.com>
2019-02-02 17:10:09 +0000

    Removed sqlite3 from .gitignore Added relevant classes into Models Migrated change into database Created some sample records to test Registered models into admin Segregated Staff and Parent user accounts

--- EduOne/main/admin.py
+++ EduOne/main/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-#from .models import Test
+from .models import *
 
 # Register your models here.
 # Example Below
-# admin.site.register(Test)
+admin.site.register(ParentProfile)
+admin.site.register(StaffProfile)
+admin.site.register(Student)
+admin.site.register(StudentToStaffSubject)
+admin.site.register(ReportCardPage)
+admin.site.register(SubjectGrade)
+admin.site.register(Comment)
