a8da061d33ee3384b90dc82a07d141884c594527
ibenrawkss <benben-x3@hotmail.com>
2019-02-04 07:26:03 +0000

    Added Event, EventPlanner and Appointment into Models Added back ReportCard class for more abstraction

--- EduOne/main/models.py
+++ EduOne/main/models.py
@@ -8,7 +8,10 @@ class ParentProfile(models.Model):
         ('Mother', 'Mother'),
     )
 
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(
+        User, 
+        on_delete=models.CASCADE
+    )
     contact_number = models.CharField(max_length=20)
     relation = models.CharField(max_length=6, choices=RELATION_CHOICES)
 
@@ -22,6 +25,7 @@ class StaffProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     contact_number = models.CharField(max_length=20)
 
+
 class Student(models.Model):
     child_of = models.ForeignKey(
         'ParentProfile', 
@@ -34,35 +38,89 @@ class Student(models.Model):
     home_address = models.CharField(max_length=100)
     emergency_contact = models.CharField(max_length=20)
 
+
 class StudentToStaffSubject(models.Model):
-    staff = models.ForeignKey('StaffProfile', on_delete=models.CASCADE)
-    student = models.ForeignKey('Student', on_delete=models.CASCADE)
+    # Create Composite key (staff,student)
+    staff = models.ForeignKey(
+        'StaffProfile', 
+        on_delete=models.CASCADE
+    )
+    student = models.ForeignKey(
+        'Student', 
+        on_delete=models.CASCADE
+    )
     subjectName = models.CharField(max_length=50)
 
+class ReportCard(models.Model):
+    student = models.OneToOneField(
+        'Student', 
+        on_delete=models.CASCADE
+    )
+
 class ReportCardPage(models.Model):
-    student = models.ForeignKey('Student', on_delete=models.CASCADE)
+    reportCard = models.ForeignKey(
+        'ReportCard',
+        on_delete=models.CASCADE
+    )
     examination_type = models.CharField(max_length=20)
     exam_date = models.DateField()
     description = models.CharField(max_length=200)
     acknowledgement = models.BooleanField(default=False)
 
+
 class SubjectGrade(models.Model):
-    reportCardPage = models.ForeignKey('ReportCardPage', on_delete=models.CASCADE)
+    reportCardPage = models.ForeignKey(
+        'ReportCardPage', 
+        on_delete=models.CASCADE
+    )
     subjectName = models.CharField(max_length=50)
     marks = models.DecimalField(max_digits=3, decimal_places=1)
 
+
 class Comment(models.Model):
-    student = models.ForeignKey('Student', on_delete=models.CASCADE)
+    student = models.ForeignKey(
+        'Student', 
+        on_delete=models.CASCADE
+    )
     commentBy = models.CharField(max_length=50)
     commentDate = models.DateField(auto_now=True)
     commentTime = models.TimeField(auto_now=True)
     comment = models.CharField(max_length=200)
 
+
 class Attendance(models.Model):
     pass
 
+
 class EventPlanner(models.Model):
-    pass
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,    
+    )
+
 
 class Event(models.Model):
-    pass
+    eventPlanner = models.ForeignKey(
+        'EventPlanner',
+        on_delete=models.CASCADE,
+    )
+    eventDate = models.DateField()
+    timeFrom = models.TimeField()
+    timeTo = models.TimeField()
+    description = models.CharField(max_length=200, blank=True, null=True)
+
+class Appointment(models.Model):
+    eventPlanner = models.ForeignKey(
+        'EventPlanner',
+        on_delete=models.CASCADE,
+    )
+    parent = models.ForeignKey(
+        'ParentProfile',
+        on_delete=models.CASCADE,
+    )
+    apptDate = models.DateField()
+    apptTimeFrom = models.TimeField()
+    apptTimeTo = models.TimeField()
+    apptDescription = models.CharField(max_length=200)
+    approved = models.BooleanField(default=False)
+
