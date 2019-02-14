abcd65f2e44fc33292f2628f24f9448a1a1bf3e9
Ben <benben-x3@hotmail.com>
2019-02-14 13:36:38 +0800

    Models (#4)
    * Modified helpfile content
    
    * Removed sqlite3 from .gitignore
    Added relevant classes into Models
    Migrated change into database
    Created some sample records to test
    Registered models into admin
    Segregated Staff and Parent user accounts
    
    * Added Event, EventPlanner and Appointment into Models
    Added back ReportCard class for more abstraction
    
    * Added Attendance into model
    Implemented composite key for Attendance and StudentToStaffProject in model
    updated .gitignore file to ignore migration files and vs code env files
    
    * modified models with relevant tables
    cleaned database with new records

--- EduOne/main/admin.py
+++ EduOne/main/admin.py
@@ -4,9 +4,14 @@ from .models import *
 # Register your models here.
 # Example Below
 admin.site.register(ParentProfile)
+admin.site.register(Class)
+admin.site.register(SubjectClass)
 admin.site.register(StaffProfile)
 admin.site.register(Student)
-admin.site.register(StudentToStaffSubject)
+admin.site.register(StudentToSubjectClass)
 admin.site.register(ReportCardPage)
 admin.site.register(SubjectGrade)
 admin.site.register(Comment)
+admin.site.register(Attendance)
+admin.site.register(Event)
+admin.site.register(EventPlanner)
--- EduOne/main/models.py
+++ EduOne/main/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.timezone import now
 
 # Create your models here.
 class ParentProfile(models.Model):
@@ -8,61 +9,221 @@ class ParentProfile(models.Model):
         ('Mother', 'Mother'),
     )
 
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(
+        User, 
+        on_delete=models.CASCADE,
+    )
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
     contact_number = models.CharField(max_length=20)
     relation = models.CharField(max_length=6, choices=RELATION_CHOICES)
 
+    def __str__(self):
+        return "{} {}".format(self.lastname, self.firstname)
+
+
+class Class(models.Model):
+    className = models.CharField(max_length=10)
+
+    def __str__(self):
+        return "Class: {}".format(self.className)
+
+
+class SubjectClass(models.Model):
+    classOf = models.OneToOneField(
+        'Class',
+        on_delete=models.CASCADE,
+    )
+    student = models.ManyToManyField(
+        'Student',
+        through='StudentToSubjectClass',
+        through_fields=('subjectClass', 'student'),
+    )
+    teacher = models.ForeignKey(
+        'StaffProfile',
+        null=True,
+        on_delete=models.SET_NULL,
+    )
+    subject = models.CharField(max_length=20)
+
+    def __str__(self):
+        return "Class: {}, Subject: {}".format(self.classOf, self.subject)
+
 
 class StaffProfile(models.Model):
-    staff_of = models.ManyToManyField(
-        'Student', 
-        through='StudentToStaffSubject', 
-        through_fields=('staff', 'student'),
+
+    user = models.OneToOneField(
+        User, 
+        on_delete=models.CASCADE
     )
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    form_class = models.ForeignKey(
+        'Class',
+        null=True,
+        on_delete=models.SET_NULL,
+    )
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
     contact_number = models.CharField(max_length=20)
+
+    def __str__(self):
+        return "{} {}".format(self.lastname, self.firstname)
+
 
 class Student(models.Model):
     child_of = models.ForeignKey(
         'ParentProfile', 
         null=True, 
-        on_delete=models.SET_NULL
+        on_delete=models.SET_NULL,
     )
+    form_class = models.ForeignKey(
+        'Class',
+        null=True,
+        on_delete=models.SET_NULL,
+    )
+    
     nric = models.CharField(max_length=9, primary_key=True)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     home_address = models.CharField(max_length=100)
     emergency_contact = models.CharField(max_length=20)
 
-class StudentToStaffSubject(models.Model):
-    staff = models.ForeignKey('StaffProfile', on_delete=models.CASCADE)
-    student = models.ForeignKey('Student', on_delete=models.CASCADE)
-    subjectName = models.CharField(max_length=50)
+    def __str__(self):
+        return "{} {}".format(self.last_name, self.first_name)
+
+
+class StudentToSubjectClass(models.Model):
+    subjectClass = models.ForeignKey(
+        'SubjectClass', 
+        on_delete=models.CASCADE,
+    )
+    student = models.ForeignKey(
+        'Student', 
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return "{} {}".format(self.subjectClass, self.student)
+
+    class Meta:
+        # all db options go here; sort, order by, index etc..
+        unique_together = ("subjectClass", "student")
+
+
+class ReportCard(models.Model):
+    student = models.OneToOneField(
+        'Student', 
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return "{}'s RCard".format(self.student)
+
 
 class ReportCardPage(models.Model):
-    student = models.ForeignKey('Student', on_delete=models.CASCADE)
+    reportCard = models.ForeignKey(
+        'ReportCard',
+        on_delete=models.CASCADE,
+    )
     examination_type = models.CharField(max_length=20)
     exam_date = models.DateField()
     description = models.CharField(max_length=200)
     acknowledgement = models.BooleanField(default=False)
 
+    def __str__(self):
+        return "{} | {} | {}".format(self.examination_type, self.exam_date, self.reportCard)
+
+
 class SubjectGrade(models.Model):
-    reportCardPage = models.ForeignKey('ReportCardPage', on_delete=models.CASCADE)
+    reportCardPage = models.ForeignKey(
+        'ReportCardPage', 
+        on_delete=models.CASCADE,
+    )
     subjectName = models.CharField(max_length=50)
     marks = models.DecimalField(max_digits=3, decimal_places=1)
 
+    def __str__(self):
+        return "{} {}".format(self.reportCardPage, self.subjectName)
+
+
 class Comment(models.Model):
-    student = models.ForeignKey('Student', on_delete=models.CASCADE)
+    student = models.ForeignKey(
+        'Student', 
+        on_delete=models.CASCADE,
+    )
     commentBy = models.CharField(max_length=50)
-    commentDate = models.DateField(auto_now=True)
-    commentTime = models.TimeField(auto_now=True)
+    commentDate = models.DateField(default=now())
+    commentTime = models.TimeField(default=now())
     comment = models.CharField(max_length=200)
 
+    def __str__(self):
+        return "Comment by: {} for {}".format(self.commentBy, self.student)
+
+
 class Attendance(models.Model):
-    pass
+    student = models.ForeignKey(
+        'Student',
+        on_delete=models.CASCADE,
+    )
+    date = models.DateField(default=now())
+
+    def __str__(self):
+        return "{} | {}".format(self.date, self.student)
+    
+    class Meta:
+        unique_together = ("student", "date")
+
 
 class EventPlanner(models.Model):
-    pass
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,    
+    )
+
+    def __str__(self):
+        return "{}'s Planner".format(self.user)
+
 
 class Event(models.Model):
-    pass
+    eventPlanner = models.ForeignKey(
+        'EventPlanner',
+        on_delete=models.CASCADE,
+    )
+    eventDate = models.DateField()
+    timeFrom = models.TimeField()
+    timeTo = models.TimeField()
+    description = models.CharField(max_length=200, blank=True, null=True)
+
+    def __str__(self):
+        return "{}, Date: {}, From: {}, To: {}".format(self.eventPlanner, self.eventDate, self.timeFrom, self.timeTo)
+
+
+class Appointment(models.Model):
+    eventPlanner = models.ForeignKey(
+        'EventPlanner',
+        on_delete=models.CASCADE,
+    )
+    parent = models.ForeignKey(
+        'ParentProfile',
+        on_delete=models.CASCADE,
+    )
+    apptDate = models.DateField()
+    apptTimeFrom = models.TimeField()
+    apptTimeTo = models.TimeField()
+    apptDescription = models.CharField(max_length=200)
+    approved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "Appt by: {} | Approved: {}".format(self.parent, self.approved)
+
+
+class Announcement(models.Model):
+    creator = models.CharField(max_length=50)
+    description = models.CharField(max_length=500)
+    isEvent = models.BooleanField(default=False)
+    eventDate = models.DateField()
+    eventTimeFrom = models.TimeField()
+    eventTimeTo = models.TimeField()
+
+    def __str__(self):
+        return "Creator: {}, isEvent: {}".format(self.creator, self.isEvent)
+
