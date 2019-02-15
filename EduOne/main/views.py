125047d16e89a1feace343cfd622f389cc32740e
ibenrawkss <benben-x3@hotmail.com>
2019-02-15 07:27:13 +0000

    Added frontend skeleton Added css and js framework Updated django version Modified requirements.txt Modified Pipfile Modified Pipfile.lock Added index page template

--- EduOne/main/views.py
+++ EduOne/main/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .models import ReportCardPage, SubjectGrade
 
 # Create your views here.
-def main(request):
-    return render(request, '/', 'hello')
+def home(request):
+    return render(request, 'index.html', {'active_page': 'home'})
