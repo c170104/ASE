c8e3e0f05172e1d96b3b332fea35b024cf429e75
Kaizer <cyberavenz@gmail.com>
2019-03-04 16:38:07 +0800

    4/3/19 updates
    ***Commented in the models.py with some issues!***
    Added 2 form classes in model
    
    Schedule Skeleton is up
    Add Event (Events, Announcements)
    Manage Events (Different functions for pending and confimed / announcements)

--- EduOne/main/models.py
+++ EduOne/main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.forms import ModelForm
 from django.contrib.auth.models import User
 from django.utils.timezone import now
 
@@ -188,14 +189,41 @@ class Event(models.Model):
         'EventPlanner',
         on_delete=models.CASCADE,
     )
+    #Maybe can include an event title
+    #Events are a one day thing? Because there is no eventEndDate
+    description = models.CharField(max_length=200, blank=True, null=True)
     eventDate = models.DateField()
     timeFrom = models.TimeField()
     timeTo = models.TimeField()
-    description = models.CharField(max_length=200, blank=True, null=True)
+    #Need a location field here that is similar to description
 
     def __str__(self):
         return "{}, Date: {}, From: {}, To: {}".format(self.eventPlanner, self.eventDate, self.timeFrom, self.timeTo)
 
+#This form is used by the views.schedule_add to add create new Event objects
+class EventForm(ModelForm):
+    class Meta:
+        model = Event
+        exclude = ['eventPlanner']
+        labels = {
+            'description': ('Event Description'),
+            'eventDate': ('Date of Event'),
+            'timeFrom': ('Start Time of Event'),
+            'timeTo': ('End Time of Event'),
+        }
+        help_texts = {
+            'description' : ('The details of the event goes here.'),
+            'eventDate' : ('The date of the event goes here.'),
+            'timeFrom' : ('The start time of the event goes here.'),
+            'timeTo' : ('The end time of the event goes here.'),
+        }
+
+
+APPOINTMENT_STATUS = (
+    ('pending'),
+    ('approved'),
+    ('rejected')
+    )
 
 class Appointment(models.Model):
     eventPlanner = models.ForeignKey(
@@ -220,6 +248,7 @@ class Announcement(models.Model):
     creator = models.CharField(max_length=50)
     description = models.CharField(max_length=500)
     isEvent = models.BooleanField(default=False)
+    #Announcements can cover only a single day? Because there is no eventEndDate
     eventDate = models.DateField()
     eventTimeFrom = models.TimeField()
     eventTimeTo = models.TimeField()
@@ -227,3 +256,22 @@ class Announcement(models.Model):
     def __str__(self):
         return "Creator: {}, isEvent: {}".format(self.creator, self.isEvent)
 
+#This form is used by the views.schedule_add to add create new Announcement objects
+class AnnoucementForm(ModelForm):
+    class Meta:
+        model = Announcement
+        exclude = ['creator']
+        labels = {
+            'description': ('Announcement Description'),
+            'isEvent' : ('Is this announcement part of a event?'),
+            'eventDate': ('Date of Announcement'),
+            'eventTimeFrom': ('Start Time of Announcement'),
+            'eventTimeTo': ('End Time of Announcement'),
+        }
+        help_texts = {
+            'description': ('The announcement description goes here.'),
+            'isEvent' : ('Specifies whether a not the announcement is part of a event.'),
+            'eventDate': ('The date of the announcement goes here.'),
+            'eventTimeFrom': ('The time where the announcement\'s event(s) starts goes here.'),
+            'eventTimeTo': ('The time where the announcement\'s event(s) ends goes here.'),
+        }
--- EduOne/main/views.py
+++ EduOne/main/views.py
@@ -1,32 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .models import ReportCardPage, SubjectGrade
+from .models import ReportCardPage, SubjectGrade, EventForm, AnnoucementForm
 
 # Create your views here.
 def home(request):
     return render(request, 'index.html', {'active_page': 'home'})
 
 
-
+#This function is for the main page of scheduling, can be used by both teachers and parents
 #@login_required
 def schedule(request):
-	return render(request, 'schedule.html', {'active_page': 'schedule'})
+	return render(request, 'schedule/schedule.html', {'active_page': 'schedule'})
 
+#This function is for the teachers to add new event that can be announcments/events
+#@login_required
+def schedule_add(request, current='events'):
+	event_categories = ('events', 'announcements')
+	if current not in event_categories:
+		#The warning below can be used for bootstrap to call messages which will display as a warning with the message
+		messages.warning(request, f'Invalid path indicated to {current}!')
+		#Redirects user to the add page to add new events if the parameters does not meet the tabs we have
+		return redirect ('schedule-add')
+	
+	elif current == event_categories[0]:
+		form = EventForm()
+		#Create objects to add into classes!
 
+	elif current == event_categories[1]:
+		form = AnnoucementForm()
+		#Create objects to add into classes!
+	
+	return render(request, 'schedule/schedule_add.html', {'active_page': 'schedule', 'active_tab': current, 'form' : form})
 
-def schedule_add(request):
-	return render(request, 'schedule.html', {'active_page': 'schedule'})
-
-
+#This function is used for manage scheduling page
+#@login_required
 def schedule_manage(request, current='confirmed'):
-	if current == 'pending':
-		#Pull the list of appointments awaiting approval
-		return render(request, 'schedule_manage.html', {'active_page': 'schedule', 'active_tab': current})
-
-	elif current == 'announcements':
-		#Pull the list of announcements available
-		return render(request, 'schedule_manage.html', {'active_page': 'schedule', 'active_tab': current})
+	event_types = ('confirmed', 'pending', 'announcements')
+	if current not in event_types:
+		#The warning below can be used for bootstrap to call messages which will display as a warning with the message
+		messages.warning(request, f'Invalid path indicated to {current}!')
+		#Redirects user to the manage page with the active tab being confirmed if the parameters does not meet the tabs we have
+		return redirect ('schedule-manage')
 
 	else:
-		return render(request, 'schedule_manage.html', {'active_page': 'schedule', 'active_tab': current})
+		#Pull the relevant data and return
+		return render(request, 'schedule/schedule_manage.html', {'active_page': 'schedule', 'active_tab': current})
+
+
+def event_delete(request, username, eventid):
+	#Pull the data and return + process the form
+	return render(request, 'events_delete.html', {'active_page': 'schedule'})
 
