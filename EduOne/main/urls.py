4bbe2041d1086b626b2df84ba3edcf029070dda8
Kaizer <cyberavenz@gmail.com>
2019-03-05 16:40:33 +0800

    5/3/19
    The manage schedule is done, able to pull schedules for pending, confirmed and announcements.

--- EduOne/main/urls.py
+++ EduOne/main/urls.py
@@ -18,14 +18,18 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views, forms
 
+
 urlpatterns = [
     path('', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('home/', views.home, name='main-home'),
     path('schedule/', views.schedule, name='schedule-home' ),
+    #Might need to edit
     path('schedule/add/', views.schedule_add, name='schedule-add'),
     path('schedule/add=<slug:current>/', views.schedule_add, name='schedule-add'),
     path('schedule/manage/', views.schedule_manage, name='schedule-manage'),
     path('schedule/manage=<slug:current>/', views.schedule_manage, name='schedule-manage'),
     path('schedule/manage/delete', views.event_delete, name='event-delete'),
+    #Trials
+    
 ]
--- EduOne/main/views.py
+++ EduOne/main/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .models import ReportCardPage, SubjectGrade
-from .forms import EventForm, AnnoucementForm
+from .models import *
+from .forms import *
+
 
 # Create your views here.
+
+#Returns the home page for display (left the calendar to display)
 @login_required
 def home(request):
-    return render(request, 'index.html', {'active_page': 'home'})
+	events = Announcement.objects.all()
+	events = events.extra(order_by = ['-eventDate'])
+	return render(request, 'index.html', {'active_page': 'home', 'announcements' : events })
 
 
 #This function is for the main page of scheduling, can be used by both teachers and parents
@@ -15,7 +20,7 @@ def home(request):
 def schedule(request):
 	return render(request, 'schedule/schedule.html', {'active_page': 'schedule'})
 
-#This function is for the teachers to add new event that can be announcments/events
+#This function is for the teachers to add new event that can be announcments/events (not completed)
 @login_required
 def schedule_add(request, current='events'):
 	event_categories = ('events', 'announcements')
@@ -36,21 +41,38 @@ def schedule_add(request, current='events'):
 	
 	return render(request, 'schedule/schedule_add.html', {'active_page': 'schedule', 'active_tab': current, 'form' : form})
 
-#This function is used for manage scheduling page
-#@login_required
+#This function is used for displaying and managing schedules for teachers (Completed)
+@login_required
 def schedule_manage(request, current='confirmed'):
 	event_types = ('confirmed', 'pending', 'announcements')
 	if current not in event_types:
-		#The warning below can be used for bootstrap to call messages which will display as a warning with the message
-		messages.warning(request, f'Invalid path indicated to {current}!')
 		#Redirects user to the manage page with the active tab being confirmed if the parameters does not meet the tabs we have
 		return redirect ('schedule-manage')
 
 	else:
-		#Pull the relevant data and return
-		return render(request, 'schedule/schedule_manage.html', {'active_page': 'schedule', 'active_tab': current})
+		parent = []
+		if current == event_types[0]:
+			#Look for a planner that belongs to the current user and searches for all the events within it
+			#get method will produce error if more than 1 planner is found.
+			planner = EventPlanner.objects.get(user__exact = request.user.id)
+			events = Event.objects.filter(eventPlanner__exact = planner).values()
 
+		elif current == event_types[1]:
+			#Look for a planner that belongs to the current user and searches for all the events within it
+			#get method will produce error if more than 1 planner is found.
+			planner = EventPlanner.objects.get(user__exact = request.user.id)
+			events = Appointment.objects.filter(eventPlanner__exact = planner).values()
+			#This replaces the parent_id with the name of the parent
+			for event in events:
+				event['parent_id'] = ParentProfile.objects.get(id__exact = event['parent_id'])
+	
+		elif current == event_types[2]:
+			events = Announcement.objects.filter(creator__exact = request.user.username)
+		
+		return render(request, 'schedule/schedule_manage.html', {'active_page': 'schedule', 'active_tab': current, 
+				'events': events, 'parentDetails' : parent })
 
+#Not completed
 def event_delete(request, username, eventid):
 	#Pull the data and return + process the form
 	return render(request, 'events_delete.html', {'active_page': 'schedule'})
