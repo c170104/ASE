475f16d888daf2288922ee98f619570c14a8fcc5
cyberavenz <37392987+cyberavenz@users.noreply.github.com>
2019-03-06 20:19:07 +0800

    Wen kai branches (#10)
    * ignore
    
    IDK how to ignore this cos i added stuff into db
    
    * 5/3/19
    
    The manage schedule is done, able to pull schedules for pending, confirmed and announcements.
    
    * 6/3/19
    
    Completed (Schedule)
    Add functions are completed!
    Delete functions are working for announcements and confirmed!
    Left Edit functions
    (Student functions) left!
    
    * Update db.sqlite3
    
    * ..
    
    * 6/3/19 1900 Updates
    
    Changes:
    Database has been modified to contain new / updated fields
    Updated the respective views and forms

--- EduOne/main/forms.py
+++ EduOne/main/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.forms import ModelForm
-from .models import Event, Announcement
+from .models import Event, Announcement, EventPlanner
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import DeleteView
 
 #This form is used by the views.schedule_add to add create new Event objects
 class EventForm(ModelForm):
@@ -8,35 +10,58 @@ class EventForm(ModelForm):
         model = Event
         exclude = ['eventPlanner']
         labels = {
+            'title': ('Event Title'),
             'description': ('Event Description'),
-            'eventDate': ('Date of Event'),
+            'location': ('Location of Event'),
+            'dateFrom': ('Start Date of Event'),
+            'dateTo': ('End Date of Event'),
             'timeFrom': ('Start Time of Event'),
             'timeTo': ('End Time of Event'),
         }
         help_texts = {
+            'title':('The title of the event goes here.'),
             'description' : ('The details of the event goes here.'),
-            'eventDate' : ('The date of the event goes here.'),
+            'location': ('The location details of event goes here.'),
+            'dateFrom': ('The start date of the event goes here.'),
+            'dateTo': ('The end date of the event goes here.'),
             'timeFrom' : ('The start time of the event goes here.'),
             'timeTo' : ('The end time of the event goes here.'),
         }
 
 
 #This form is used by the views.schedule_add to add create new Announcement objects
-class AnnoucementForm(ModelForm):
+class AnnouncementForm(ModelForm):
     class Meta:
         model = Announcement
-        exclude = ['creator']
+        exclude = ['user', 'dateCreated']
         labels = {
+            'title' : ('Announcement Title'),
             'description': ('Announcement Description'),
-            'isEvent' : ('Is this announcement part of a event?'),
-            'eventDate': ('Date of Announcement'),
-            'eventTimeFrom': ('Start Time of Announcement'),
-            'eventTimeTo': ('End Time of Announcement'),
         }
         help_texts = {
+            'title' : ('The announcement title goes here.'),
             'description': ('The announcement description goes here.'),
-            'isEvent' : ('Specifies whether a not the announcement is part of a event.'),
-            'eventDate': ('The date of the announcement goes here.'),
-            'eventTimeFrom': ('The time where the announcement\'s event(s) starts goes here.'),
-            'eventTimeTo': ('The time where the announcement\'s event(s) ends goes here.'),
         }
+
+#This class form is used by announcement-delete to delete existing announcements
+class AnnouncementDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Announcement
+    success_url = "/schedule/manage=announcements/"
+    
+    def test_func(self):
+        announcement = self.get_object()
+        if self.request.user == announcement.user:
+            return True
+        return False    
+
+#This class form is used by event-delete to delete existing events
+class EventDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Event
+    success_url = "/schedule/manage=confirmed/"
+    
+    def test_func(self):
+        event = self.get_object()
+        user_id = EventPlanner.objects.get(id__exact = event.eventPlanner_id).user_id
+        if self.request.user.id == user_id:
+            return True
+        return False         
--- EduOne/main/views.py
+++ EduOne/main/views.py
@@ -1,21 +1,28 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .models import ReportCardPage, SubjectGrade
-from .forms import EventForm, AnnoucementForm
+from .models import *
+from .forms import *
+import datetime
+
+
 
 # Create your views here.
+
+#Returns the home page for display (left the calendar to display)
 @login_required
 def home(request):
-    return render(request, 'index.html', {'active_page': 'home'})
+	events = Announcement.objects.all()
+	events = events.extra(order_by = ['-dateCreated'])
+	return render(request, 'index.html', {'active_page': 'home', 'announcements' : events })
 
 
-#This function is for the main page of scheduling, can be used by both teachers and parents
+#This function is for the main page of scheduling, can be used by both teachers and parents (left the calendar to display)
 @login_required
 def schedule(request):
 	return render(request, 'schedule/schedule.html', {'active_page': 'schedule'})
 
-#This function is for the teachers to add new event that can be announcments/events
+#This function is for the teachers to add new event that can be announcments/events (completed)
 @login_required
 def schedule_add(request, current='events'):
 	event_categories = ('events', 'announcements')
@@ -25,33 +32,55 @@ def schedule_add(request, current='events'):
 		#Redirects user to the add page to add new events if the parameters does not meet the tabs we have
 		return redirect ('schedule-add')
 	
-	elif current == event_categories[0]:
-		form = EventForm(request.POST)
+	else:
+		if current == event_categories[0]:
+			form = EventForm(request.POST)
+			if form.is_valid():
+				event = form.save(commit=False)
+				event.eventPlanner = EventPlanner.objects.get(user__exact = request.user.id)
+				form.save()
+				messages.success(request, f'Event has been created successfully!')
+				return redirect('schedule-add')
 
-		#Create objects to add into classes!
-
-	elif current == event_categories[1]:
-		form = AnnoucementForm()
-		#Create objects to add into classes!
+		elif current == event_categories[1]:
+			form = AnnouncementForm(request.POST)
+			if form.is_valid():
+				announcement = form.save(commit=False)
+				announcement.user_id = request.user.id
+				announcement.dateCreated = datetime.datetime.now().date()
+				form.save()
+				messages.success(request, f'Announcement has been created successfully!')
+				return redirect('schedule-add')
 	
 	return render(request, 'schedule/schedule_add.html', {'active_page': 'schedule', 'active_tab': current, 'form' : form})
 
-#This function is used for manage scheduling page
-#@login_required
+#This function is used for displaying and managing schedules for teachers (Completed)
+@login_required
 def schedule_manage(request, current='confirmed'):
 	event_types = ('confirmed', 'pending', 'announcements')
 	if current not in event_types:
-		#The warning below can be used for bootstrap to call messages which will display as a warning with the message
-		messages.warning(request, f'Invalid path indicated to {current}!')
 		#Redirects user to the manage page with the active tab being confirmed if the parameters does not meet the tabs we have
 		return redirect ('schedule-manage')
 
 	else:
-		#Pull the relevant data and return
-		return render(request, 'schedule/schedule_manage.html', {'active_page': 'schedule', 'active_tab': current})
+		if current == event_types[0]:
+			#Look for a planner that belongs to the current user and searches for all the events within it
+			#get method will produce error if more than 1 planner is found.
+			planner = EventPlanner.objects.get(user__exact = request.user.id)
+			events = Event.objects.filter(eventPlanner__exact = planner)
 
+		elif current == event_types[1]:
+			#Look for a planner that belongs to the current user and searches for all the events within it
+			#get method will produce error if more than 1 planner is found.
+			planner = EventPlanner.objects.get(user__exact = request.user.id)
+			events = Appointment.objects.filter(eventPlanner__exact = planner).values()
+			#This replaces the parent_id with the name of the parent
+			for event in events:
+				event['parent_id'] = ParentProfile.objects.get(id__exact = event['parent_id'])
+	
+		elif current == event_types[2]:
+			events = Announcement.objects.filter(user__exact = request.user.id)
 
-def event_delete(request, username, eventid):
-	#Pull the data and return + process the form
-	return render(request, 'events_delete.html', {'active_page': 'schedule'})
+		return render(request, 'schedule/schedule_manage.html', {'active_page': 'schedule', 'active_tab': current, 
+				'events': events })
 
